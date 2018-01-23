036843b82adcf9a7af4a60cf9b88eb86c3e7fa24
littlechad <fian.kurniawan@gmail.com>
2018-01-23 11:39:10 +0700

    [FIX] unique key in map

--- src/elements/Hashtag.js
+++ src/elements/Hashtag.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Link } from 'react-router'
 import PropTypes from 'prop-types'
 
+import uuid from 'uuid'
+
 const Hashtag = (props) => {
   const hashtag = props.string
     .split(' ')
     .map((string) => {
       const str = string
       return str.startsWith('#')
-        ? (<Link to={`${props.url}/${str.split('#')[1]}`}>{str}</Link>)
+        ? (<Link key={uuid.v4()} to={`${props.url}/${str.split('#')[1]}`}>{str}</Link>)
         : str
     })
     .reduce((prev, curr) => [prev, ' ', curr])
